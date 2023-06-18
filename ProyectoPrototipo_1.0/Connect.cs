8f76746828cb8e7a6d33c2e9be1df1d77be32195
Jonathan Poaquiza <jonathan.poaquiza@epn.edu.ec>
2023-06-18 00:32:10 -0500

    namespace

--- ProyectoPrototipo_1.0/Connect.cs
+++ ProyectoPrototipo_1.0/Connect.cs
@@ -5,7 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Data.SqlClient;
 
-namespace Farmacia
+namespace ProyectoPrototipo_1._0
 {
 
     public class Connect
--- ProyectoPrototipo_1.0/FORMS/Form_Login.cs
+++ ProyectoPrototipo_1.0/FORMS/Form_Login.cs
@@ -1,5 +1,4 @@
-﻿using Farmacia;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
