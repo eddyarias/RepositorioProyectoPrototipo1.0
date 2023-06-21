e9b20f658b34698ee697fb77a366876ce466f9cb
Milton Pastor <milton.pastor@epn.edu.ec>
2023-06-21 03:55:43 -0500

    Inventario

--- ProyectoPrototipo_1.0/FORMS/Form_Inventario.cs
+++ ProyectoPrototipo_1.0/FORMS/Form_Inventario.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
@@ -22,7 +20,30 @@ namespace ProyectoPrototipo_1._0
             // Centrar el formulario en la pantalla
             this.StartPosition = FormStartPosition.Manual;
             this.Location = new Point((Screen.PrimaryScreen.Bounds.Width - this.Width) / 2 - 400,
-                                      (Screen.PrimaryScreen.Bounds.Height - this.Height) / 2); ;
+                                      (Screen.PrimaryScreen.Bounds.Height - this.Height) / 2);
+
+            dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.AllCells;
+            dataGridView1.Rows.Add(001, "Pasta dental", "Aseo Perosnal", 3.99, 10, "10/02/2024");
+
+            dataGridView1.CellClick += dataGridView1_CellContentClick;
+            dataGridView1.ReadOnly = true; //no editable 
+            dataGridView1.AllowUserToAddRows = false;
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            // Verifica que el evento ocurrió en una fila (y no en los encabezados o en una celda vacía)
+            if (e.RowIndex >= 0)
+            {
+                // Obtén los datos de la fila seleccionada
+                DataGridViewRow row = dataGridView1.Rows[e.RowIndex];
+                txtCodigoP.Text = row.Cells["Código"].Value.ToString();
+                txtNombreP.Text = row.Cells["Column1"].Value.ToString();
+                txtDesP.Text = row.Cells["Column3"].Value.ToString();
+                txtPrecioP.Text = row.Cells["Column2"].Value.ToString();
+                txtCantidadP.Text = row.Cells["Cantidad"].Value.ToString();
+                txtFechaCad.Text = row.Cells["Feccad"].Value.ToString();
+            }
         }
     }
 }
